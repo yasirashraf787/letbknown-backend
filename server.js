688e939cfd72e7738f90fc60ab1820deb8b6418c
Yasir Khan <58009725+yasirashraf787@users.noreply.github.com>
2021-08-14 22:08:41 -0500

    Added welcome end point

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+app.get('/', (req,res)=>{
+    res.json({"message:" : "Welcome To LetBKnown!!!"});
+});
+
+
 server.listen(port, () => {
     console.log('Server running on port:', port);
 })
